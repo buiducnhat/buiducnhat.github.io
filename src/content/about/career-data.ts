00351421c13b96434ae6868419c4b64444dedcf2
buiducnhat <nhaths4701@gmail.com>
2025-02-24 22:23:59 +0700

    Add education, career data

---
+++ src/content/about/career-data.ts
@@ -0,0 +1,36 @@
+export const CAREER_DATA = [
+  {
+    name: "SotaTek",
+    url: "https://sotatek.com",
+    date: "06/2024 - Present",
+    title: "Fullstack Developer",
+    description: [
+      "Work as Fullstack Developer for outsourcing projects",
+      "Work with clients from many countries (focusing in EU)",
+      "Business analysis, clear requirements, make documentations for products.",
+      "Base the source code for projects, find solution, architecture for complex taks, problems.",
+    ],
+  },
+  {
+    name: "Yootek Holding",
+    url: "https://yootek.com.vn",
+    date: "04/2022 - 05/2024",
+    title: "Fullstack Developer",
+    description: [
+      "Lead a team to develop an IoT system for smarthome field.",
+      "Business analysis, clear requirements and write documents for developing product.",
+      "Join and develop many core features of products from scratch.",
+      "Base the source code for projects, find solution, architecture for complex taks, problems.",
+      "Working with cloud server, deploy products to server, also setup monitoring and take responsibility for them.",
+    ],
+  },
+  {
+    name: "DevZone",
+    date: "06/2020 - 12/2021",
+    title: "Fullstack Developer",
+    description: [
+      "Develop web application for both internal and external products.",
+      "Work as the mentor of programming courses for students.",
+    ],
+  },
+];
---
+++ src/content/about/education-data.ts
@@ -0,0 +1,8 @@
+export const EDUCATION_DATA = [
+  {
+    name: "Hanoi University of Science and Technology",
+    date: "08/2019 - 10/2024",
+    title: "Eng. Computer Engineering",
+    description: "Very good GPA",
+  },
+];
